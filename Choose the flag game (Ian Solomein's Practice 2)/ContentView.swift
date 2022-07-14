26d0042a37f44c07596b2c1890cd5d3b6d959c39
Yuri Kurinnoi <yur4kur@icloud.com>
2022-07-14 10:17:57 +0400

    Initial Commit

---
+++ Choose the flag game (Ian Solomein's Practice 2)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Choose the flag game (Ian Solomein's Practice 2)
+//
+//  Created by Пользователь on 14/07/2022.
+//  Copyright © 2022 Пользователь. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
